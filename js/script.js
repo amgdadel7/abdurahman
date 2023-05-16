4ab73b44214540d17c687ab0a99d48adf30bcb02
Amjad adel <amgdadel7@gmail.com>
2023-05-16 14:41:18 +0300

    k

--- js/script.js
+++ js/script.js
@@ -160,7 +160,131 @@ $('.next').on('click', nextHandler);
 $('.prev').on('click', prevHandler);
 
 var square = '<svg x="0px" y="0px" width="1200px" height="600px" viewBox="0 0 1200 600"><rect x="0.002" y="0.499" width="1200" height="600"/></svg>'
+// document.querySelector('.c').onclick = () => {
+//     classList.add('active');
 
+// };
+let mymedia = window.matchMedia('(max-width:600px)');
+
+function firstclick(mymedia) {
+    if (mymedia.matches) {
+        window.open("www.google.com", "_self");
+
+    } else {
+        // function firstclick() {
+
+        document.querySelector('.c').classList.add('active');
+        // return scoundclick();
+        // }
+    }
+}
+
+
+
+function act1(event) {
+
+    document.getElementById("mnuit1").parentElement.classList.remove('active')
+    event.stopPropagation();
+}
+
+function continerfristscoundimage() {
+
+    return scoundclick(mymedia);
+}
+
+function scoundclick(mymedia) {
+    if (mymedia.matches) {
+        window.open("www.google.com", "_self");
+
+    } else {
+        // function () {
+
+        document.querySelector('.scound').classList.add('active');
+        // }
+    }
+}
+
+
+function act2(event) {
+
+    document.getElementById("mnuit2").parentElement.classList.remove('active')
+    event.stopPropagation();
+}
+
+function thirdclick(mymedia) {
+    if (mymedia.matches) {
+        window.open("www.google.com", "_self");
+
+    } else {
+        // function () {
+
+        document.querySelector('.third').classList.add('active');
+        // }
+    }
+}
+
+
+function act3(event) {
+
+    document.getElementById("mnuit3").parentElement.classList.remove('active')
+    event.stopPropagation();
+}
+
+function forthclick(mymedia) {
+    if (mymedia.matches) {
+        window.open("www.google.com", "_self");
+
+    } else {
+        // function () {
+
+        document.querySelector('.forth').classList.add('active');
+        // }
+    }
+}
+
+
+function act4(event) {
+
+    document.getElementById("mnuit4").parentElement.classList.remove('active')
+    event.stopPropagation();
+}
+
+function fifthclick(mymedia) {
+    if (mymedia.matches) {
+        window.open("www.google.com", "_self");
+
+    } else {
+        // function () {
+
+        document.querySelector('.fifth').classList.add('active');
+        // }
+    }
+}
+
+
+function act5(event) {
+
+    document.getElementById("mnuit5").parentElement.classList.remove('active')
+    event.stopPropagation();
+}
+// let previewBox = document.querySelector('.c');
+
+// function remfirstclick() {
+//     var remfrsele = document.querySelector('.c');
+//     remfrsele[0].classList.remove('active');
+
+//     // console.log(remfrsele);
+//     // remfrsele.classList.remove;
+// }
+
+// let preveiwContainer = document.querySelector('.service-continer');
+// let previewBox = document.querySelectorAll('.c');
+// previewBox.forEach(close => {
+//     close.querySelector('.fa-times').onclick = () => {
+//         close.classList.remove('active');
+//         // preveiwContainer.classList.remove('active')
+//     };
+// });
 
 // const post = document.getElementsByClassName("post");
 // const b = document.querySelectorAll(".box div");
