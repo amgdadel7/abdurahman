3095ad3f8ccc806993b494187a83cc42cb6b5deb
Amjad adel <amgdadel7@gmail.com>
2023-06-01 19:19:32 +0300

    32154689789

--- js/script.js
+++ js/script.js
@@ -60,12 +60,12 @@ $(function() {
         axis: "y",
         snap: '.custom-scrollbar-js',
         snapAmount: '.custom-scrollbar-js',
-        mouseWheel:true,
-        scrollType:"continuous",
+    
+      
         
         snapAmount: slideAmount,
     
-        // contentTouchScroll: 25,
+        contentTouchScroll: 25,
         theme: "rounded-dark"
     });
 });
